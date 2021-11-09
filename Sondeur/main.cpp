7218a9bd59f498f4a1d109810f3a419e41fadaa0
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2021-11-09 13:13:06 +0100

    First commit

---
+++ Sondeur/main.cpp
@@ -0,0 +1,23 @@
+//*********************************************************************************************
+//* Programme : Sondeur.cpp								Date : 19/10/2021
+//*														Dernière MAJ : 21/10/2021
+//*--------------------------------------------------------------------------------------------
+//*Programmeurs :	CAUET Clément							Classe : BTSSN2
+//*					DUVAL Kyllian
+//*					LIENARD Romain
+//*--------------------------------------------------------------------------------------------
+//*
+//* BUT : Permet d'ouvrir l'IHM.
+//*
+//*********************************************************************************************
+
+#include "Sondeur.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Sondeur w;
+    w.show();
+    return a.exec();
+}
